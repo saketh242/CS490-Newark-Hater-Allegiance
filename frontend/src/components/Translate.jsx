a79007fd183ee1d5ace9ed21ff382eb80e6fbe36
Medina Dzhatdoyev <med.dzhatdo@gmail.com>
2024-02-25 00:13:53 -0500

    Played with icons and added more languages to the dropdown

--- frontend/src/components/Translate.jsx
+++ frontend/src/components/Translate.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import CodeOutput from './CodeOutput';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightLong, faArrowRightLong, faDownload, faCopy } from '@fortawesome/free-solid-svg-icons'
 
 const Translate = () => {
   const [inputCode, setInputCode] = useState('');
@@ -21,25 +23,50 @@ const Translate = () => {
           <option value="javascript">JavaScript</option>
           <option value="java">Java</option>
           <option value="csharp">C#</option>
+          <option value="cplusplus">C++</option>
+          <option value="php">PHP</option>
+          <option value="go">Go</option>
+          <option value="ruby">Ruby</option>
+          <option value="typescript">TypeScript</option>
         </select>
-        <button onClick={translateCode}>Translate</button>
+
+        <div className="conversionArrow">
+          <FontAwesomeIcon id="icons" icon={faArrowRightLong} size="5x"/>
+          <button onClick={translateCode}>Translate</button>
+        </div>
+
         <select id="desiredLanguage">
           <option value="python">Python</option>
           <option value="javascript">JavaScript</option>
           <option value="java">Java</option>
           <option value="csharp">C#</option>
+          <option value="cplusplus">C++</option>
+          <option value="php">PHP</option>
+          <option value="go">Go</option>
+          <option value="ruby">Ruby</option>
+          <option value="typescript">TypeScript</option>
         </select>
       </div>
 
-      <div className="upload"><button>Upload</button></div>
+      <div className="upload"><button>Upload File</button></div>
 
       <div className="codeBlocks">
-        <textarea className="inputArea"
-          value={inputCode}
-          onChange={(e) => setInputCode(e.target.value)}
-          placeholder="Enter code to translate"
-        />
-        <div className="outputArea"><CodeOutput/></div>
+        <div className="src">
+          <h2>Enter code here:</h2>
+          <textarea className="inputArea"
+            value={inputCode}
+            onChange={(e) => setInputCode(e.target.value)}
+            placeholder="Enter code to translate"
+          />
+        </div>
+        <div className="dest">
+          <h2>Converted code:</h2>
+          {/* <FontAwesomeIcon icon={faDownload} />
+          <FontAwesomeIcon icon={faCopy} /> */}
+          <div className="outputArea">
+            <CodeOutput/>
+          </div>
+        </div>
       </div>
     </div>
   )
