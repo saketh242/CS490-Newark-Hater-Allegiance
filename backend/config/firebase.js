45b1786cafbc21c3a38dbd2587982d7950695efc
Wammyve <thesquidagent@gmail.com>
2024-03-03 17:17:58 -0500

    deleted:    backend/config/firebase.js 	new file:   frontend/firebase.js

--- backend/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccount");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
---
+++ frontend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD_Q1nFv4-LtjDM5SFNhjfmjV-TR9CM90",
+  authDomain: "nhagpt2.firebaseapp.com",
+  projectId: "nhagpt2",
+  storageBucket: "nhagpt2.appspot.com",
+  messagingSenderId: "775077009176",
+  appId: "1:775077009176:web:5432981d0955e7bcf3f06d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
