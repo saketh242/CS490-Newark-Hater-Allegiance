598887bbef8a2ed3b098b8ef775c2d0492a39da1
Saketh Puramsetti <89240374+saketh242@users.noreply.github.com>
2024-03-05 15:54:12 -0500

    Update Signup.test.jsx
    Add additional tests for Signup.test.jsx

--- frontend/tests/Signup.test.jsx
+++ frontend/tests/Signup.test.jsx
@@ -121,6 +121,182 @@ describe('Signup', () => {
     });
   });
 
+  it('Empty email check', async () => {
+    render(<Signup />, { wrapper: MemoryRouter });
+
+    // Simulate typing code input into the email
+    const emailTextarea = screen.getByPlaceholderText('Email');
+    fireEvent.change(emailTextarea, { target: { value: '' } });
+
+    // simulate the typing code for input into the password box and retype password
+    const passwordTextarea = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordTextarea, { target: { value: 'Test1234' } });
+
+    const rPasswordTextarea = screen.getByPlaceholderText('Retype Password');
+    fireEvent.change(rPasswordTextarea, { target: { value: 'Test1234' } });
+
+
+    // simulate names
+    const fNameTextarea = screen.getByPlaceholderText('First Name');
+    fireEvent.change(fNameTextarea, { target: { value: 'Saketh' } });
+
+    const lNameTextarea = screen.getByPlaceholderText('Last Name');
+    fireEvent.change(lNameTextarea, { target: { value: 'Puramsetti' } });
+
+    // simulating the clicking login button
+    const signUpButton = screen.getByText('Signup');
+    fireEvent.click(signUpButton);
+
+    // looking for the error message that pops up when invalid credentials are entered
+
+    await waitFor(() => {
+      // Assert that the error message is displayed
+      const errorMessage = screen.getByText('All fields are required!');
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  it('Empty password1 check', async () => {
+    render(<Signup />, { wrapper: MemoryRouter });
+
+    // Simulate typing code input into the email
+    const emailTextarea = screen.getByPlaceholderText('Email');
+    fireEvent.change(emailTextarea, { target: { value: '' } });
+
+    // simulate the typing code for input into the password box and retype password
+    const passwordTextarea = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordTextarea, { target: { value: '' } });
+
+    const rPasswordTextarea = screen.getByPlaceholderText('Retype Password');
+    fireEvent.change(rPasswordTextarea, { target: { value: 'Test1234' } });
+
+
+    // simulate names
+    const fNameTextarea = screen.getByPlaceholderText('First Name');
+    fireEvent.change(fNameTextarea, { target: { value: 'Saketh' } });
+
+    const lNameTextarea = screen.getByPlaceholderText('Last Name');
+    fireEvent.change(lNameTextarea, { target: { value: 'Puramsetti' } });
+
+    // simulating the clicking login button
+    const signUpButton = screen.getByText('Signup');
+    fireEvent.click(signUpButton);
+
+    // looking for the error message that pops up when invalid credentials are entered
+
+    await waitFor(() => {
+      // Assert that the error message is displayed
+      const errorMessage = screen.getByText('All fields are required!');
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  it('Empty password2 check', async () => {
+    render(<Signup />, { wrapper: MemoryRouter });
+
+    // Simulate typing code input into the email
+    const emailTextarea = screen.getByPlaceholderText('Email');
+    fireEvent.change(emailTextarea, { target: { value: '' } });
+
+    // simulate the typing code for input into the password box and retype password
+    const passwordTextarea = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordTextarea, { target: { value: 'Test1234' } });
+
+    const rPasswordTextarea = screen.getByPlaceholderText('Retype Password');
+    fireEvent.change(rPasswordTextarea, { target: { value: '' } });
+
+
+    // simulate names
+    const fNameTextarea = screen.getByPlaceholderText('First Name');
+    fireEvent.change(fNameTextarea, { target: { value: 'Saketh' } });
+
+    const lNameTextarea = screen.getByPlaceholderText('Last Name');
+    fireEvent.change(lNameTextarea, { target: { value: 'Puramsetti' } });
+
+    // simulating the clicking login button
+    const signUpButton = screen.getByText('Signup');
+    fireEvent.click(signUpButton);
+
+    // looking for the error message that pops up when invalid credentials are entered
+
+    await waitFor(() => {
+      // Assert that the error message is displayed
+      const errorMessage = screen.getByText('All fields are required!');
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  it('Empty first name check', async () => {
+    render(<Signup />, { wrapper: MemoryRouter });
+
+    // Simulate typing code input into the email
+    const emailTextarea = screen.getByPlaceholderText('Email');
+    fireEvent.change(emailTextarea, { target: { value: 'saketh.puramsetti@gmail.com' } });
+
+    // simulate the typing code for input into the password box and retype password
+    const passwordTextarea = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordTextarea, { target: { value: 'Test1234' } });
+
+    const rPasswordTextarea = screen.getByPlaceholderText('Retype Password');
+    fireEvent.change(rPasswordTextarea, { target: { value: '' } });
+
+
+    // simulate names
+    const fNameTextarea = screen.getByPlaceholderText('First Name');
+    fireEvent.change(fNameTextarea, { target: { value: '' } });
+
+    const lNameTextarea = screen.getByPlaceholderText('Last Name');
+    fireEvent.change(lNameTextarea, { target: { value: 'Puramsetti' } });
+
+    // simulating the clicking login button
+    const signUpButton = screen.getByText('Signup');
+    fireEvent.click(signUpButton);
+
+    // looking for the error message that pops up when invalid credentials are entered
+
+    await waitFor(() => {
+      // Assert that the error message is displayed
+      const errorMessage = screen.getByText('All fields are required!');
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  it('Empty last name check', async () => {
+    
+    render(<Signup />, { wrapper: MemoryRouter });
+
+    // Simulate typing code input into the email
+    const emailTextarea = screen.getByPlaceholderText('Email');
+    fireEvent.change(emailTextarea, { target: { value: 'saketh.puramsetti@gmail.com' } });
+
+    // simulate the typing code for input into the password box and retype password
+    const passwordTextarea = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordTextarea, { target: { value: 'Test1234' } });
+
+    const rPasswordTextarea = screen.getByPlaceholderText('Retype Password');
+    fireEvent.change(rPasswordTextarea, { target: { value: '' } });
+
+
+    // simulate names
+    const fNameTextarea = screen.getByPlaceholderText('First Name');
+    fireEvent.change(fNameTextarea, { target: { value: 'Saketh' } });
+
+    const lNameTextarea = screen.getByPlaceholderText('Last Name');
+    fireEvent.change(lNameTextarea, { target: { value: '' } });
+
+    // simulating the clicking login button
+    const signUpButton = screen.getByText('Signup');
+    fireEvent.click(signUpButton);
+
+    // looking for the error message that pops up when invalid credentials are entered
+
+    await waitFor(() => {
+      // Assert that the error message is displayed
+      const errorMessage = screen.getByText('All fields are required!');
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
   
 
 
