82911a15d5b27b2d1d668e1495dd3aefa5c0117d
saketh242 <saketh.puramsetti@outlook.com>
2024-03-12 18:35:36 -0400

    Add hover navbar and delete functonality

--- web/src/components/DeleteAccount.jsx
+++ web/src/components/DeleteAccount.jsx
@@ -1,40 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { isValidPassword } from '../utils/fieldValidations'
-import { reauthenticateWithCredential, EmailAuthProvider, updatePassword, signOut } from 'firebase/auth';
+import { reauthenticateWithCredential, EmailAuthProvider, updatePassword, signOut, deleteUser } from 'firebase/auth';
 import { auth } from '../firebase';
 import { toast } from 'react-toastify';
 import useAuth from '../useAuth';
+import spongecry from "../memeGifs/crysponge.gif"
 
 const DeleteAccount = () => {
 
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
-    const {user, isLoading} = useAuth()
-
+    const { user, isLoading } = useAuth()
+    const navigate = useNavigate()
+    const [deleted, setDeleted] = useState(false);
+   
     const handleDelete = async (e) => {
         e.preventDefault()
-        if (password == ""){
+        if (password == "") {
             setError("Please enter your password");
             return
         }
 
         try {
+            
             // all field validations pass now time to reauthenticate user and delete account :(
             const credential = EmailAuthProvider.credential(user.email, password);
             await reauthenticateWithCredential(user, credential)
             console.log("re authentication successful")
-        } catch((e) => {
-            console.log(e)
-        }) 
- 
-s
+            // now deleting the user
+            setDeleted(true);
+            setTimeout(async () => {
+                await deleteUser(user);
+                console.log("User deleted!")
+                signOut(auth).then(() => {
+                    const msg = () => toast(`Account deleted successfully!`);
+                    msg()
+                    navigate("/");
+                    console.log("Account deleted and redirected successfully and redirected successfully")
+                }).catch((error) => {
+                    console.log(error)
+                });
+            }, 3000)
 
+        } catch (e) {
+            console.log(e.message)
+            if (e.message == "Firebase: Error (auth/invalid-credential).") {
+                setError("Wrong password, try again :(")
+            } else {
+                setError("An error occured try again")
+            }
+        }
     }
-
     return (
+        
         <div className="delete-account-div">
-
+            
+            {!deleted && (
+                <>
+            
             <h2 className="delete-heading">Deleted accounts cannot be recovered again!</h2>
             <form className="delete-form">
                 <input
@@ -51,9 +75,25 @@ s
                     }}
                 />
 
-                <button type="submit" onClick={handleDelete} className="login-btn">Delete Account</button>
+                <button
+                disabled={deleted}
+                 type="submit"
+                  onClick={handleDelete} 
+                  className="login-btn"
+                  >Delete Account</button>
             </form>
             {error && <p className="error-msg">{error}</p>}
+            
+        </>
+            )}
+            {deleted &&  (
+                <>
+                    <p className='bye-message'>Sorry to see you go :(</p>
+                </>
+
+            )}
+        <img style={{display: deleted?'block':'none'}} className='meme2' src={spongecry} alt="spongebob crying"></img>
+
         </div>
     )
 }
