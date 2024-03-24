0d5ec78fca16d77df97d802825cfa80257301006
Medina Dzhatdoyev <med.dzhatdo@gmail.com>
2024-03-24 14:59:55 -0400

    Homepage reviews updated

--- web/src/components/HomeReviews.jsx
+++ web/src/components/HomeReviews.jsx
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import nhaService from '../services/nhaService';
 
 const HomeReviews = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    async function fetchReviews() {
+      try {
+        const response = await nhaService.getFeedback();
+        const extractedReviews = response.map(item => ({
+          textMessage: item.textMessage,
+          user: `${item.user.firstName} ${item.user.lastName}`
+        }));
+        setReviews(extractedReviews);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    }
+
+    fetchReviews();
+  }, []);
+
+  // Get 5 random reviews with text messages of 100 characters or less
+  const randomReviews = reviews
+    .filter(review => review.textMessage.length <= 100)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 5);
+
   return (
-    <div className='box reviews'>
-    <p id="reviewHeader" className="sectionHeader">Some of our reviews</p>
-    <div className="reviews-flexbox">
-      <div id='review'><p>⭐⭐⭐⭐⭐<br></br>I like this app<br></br>- John Doe</p></div>
-      <div id='review'><p>⭐⭐⭐⭐⭐<br></br>this is really helpful for school<br></br>- NJIT student</p></div>
-      <div id='review'><p>⭐⭐⭐⭐⭐<br></br>I hate newark<br></br>- #1NewarkHater</p></div>
-      <div id='review'><p>⭐⭐⭐⭐⭐<br></br>Accurate and reliable!<br></br>- Jane</p></div>
+    <div className='box reviews' id="reviewContainer">
+      <p id="reviewHeader" className="sectionHeader">Some of our reviews</p>
+      <div className="reviews-flexbox">
+        <Carousel className="homeCarousel" infiniteLoop showStatus={false}>
+          {randomReviews && randomReviews.length > 0 ? (
+            randomReviews.map((review, index) => (
+              <div className="review" key={index}>
+                <p>{`⭐⭐⭐⭐⭐`}</p>
+                <p>{`${review.textMessage}`}</p>
+                <p>{`- ${review.user}`}</p>
+              </div>
+            ))
+          ) : (
+            <p>Loading reviews...</p>
+          )}
+        </Carousel>
+      </div>
     </div>
-  </div>
   );
 };
 
 export default HomeReviews;
-
-// import React, { useState, useEffect } from 'react';
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from 'react-responsive-carousel';
-
-// import nhaService from '../services/nhaService';
-// import useAuth from '../useAuth';
-
-// const HomeReviews = () => {
-//   const { user } = useAuth();
-//   const [reviews, setReviews] = useState([]);
-
-//     useEffect(() => {
-//       async function fetchReviews() {
-//         try {
-//           const response = response = await nhaService.getFeedback(user);
-//           setReviews(response.data);
-//           console.log(reviews);
-//         } catch (error) {
-//           console.error('Error fetching reviews:', error);
-//         }
-//       }
-  
-//       fetchReviews();
-//     }, []);
-  
-//     return (
-//       <div className='box reviews'>
-//         <p id="reviewHeader" className="sectionHeader">Some of our reviews</p>
-//         <div className="reviews-flexbox">
-//           {reviews.map((review, index) => (
-//             <div className="review" id={`review-${index}`} key={index}>
-//               <p>{`⭐⭐⭐⭐⭐<br></br>${review.textMessage}<br></br>- ${review.user}`}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   };
-
-// export default HomeReviews;
