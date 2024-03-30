93d4c12538cb222150b02ed4267693bffcc4478d
Medina Dzhatdoyev <med.dzhatdo@gmail.com>
2024-03-30 12:38:41 -0400

    Added styling for emtpy history sidebar

--- web/src/components/History.jsx
+++ web/src/components/History.jsx
@@ -4,6 +4,7 @@ import 'react-modern-drawer/dist/index.css'
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import emptybox from '../images/emptybox.png';
 
 const sideBarStyle = {
   backgroundColor: "#23262F",
@@ -70,16 +71,18 @@ const History = ({ history, showSidebar, toggleSidebar, setInputCode, setTransla
   });
 
   const [ascend, setAscend] = useState(-1);
-  
+
   const [sortField, setSortField] = useState("");
   const changeSort = (e) => {
     setSortField(e.target.value);
-  } 
+  }
 
   const [filterField, setFilterField] = useState("");
   const changeFilter = (e) => {
     setFilterField(e.target.value);
   }
+
+  console.log(`History: ${history}`);
 
   if (history === null || showSidebar === false) return (<></>);
   return (
@@ -88,11 +91,9 @@ const History = ({ history, showSidebar, toggleSidebar, setInputCode, setTransla
         open={showSidebar}
         onClose={toggleSidebar}
         direction='right'
-        // size={width * 0.3}
         size={width * 0.33}
         style={sideBarStyle}
       >
-
         <div className="historyTop">
           <h1 className="translationTitle"> Translation History </h1>
           <button id="closeHistory" onClick={toggleSidebar}>
@@ -100,70 +101,82 @@ const History = ({ history, showSidebar, toggleSidebar, setInputCode, setTransla
           </button>
         </div>
 
-        <div className="historyOptions">
-          {/* sort asc/desc */}
-          <button>
-            <FontAwesomeIcon id="ascdsc" icon={ascend === 1 ? faArrowUp : faArrowDown} onClick = {() => setAscend(ascend * -1)}/>
-          </button>
-
-        {/* sort by */}
-          <select id="sort" onChange={changeSort}>
-            <option value=""> Sort By... </option>
-            <option value="Date"> Date </option>
-            <option value="Source"> Source Language </option>
-            <option value="Destination"> Destination Language </option>
-          </select>
-
-        {/* filter by */}
-          <select id="filter" onChange={changeFilter}>
-            <option value=""> Filter By... </option>
-            <option value="Date"> Date </option>
-            <option value="Source"> Source Language </option>
-            <option value="Destination"> Destination Language </option>
-          </select>
-
-          {/* clear all history */}
-          <button id="clearAll" className="ripple">Clear all history</button>
-        </div>
-
-        {history.map((historyLabel, i) => (
-          <div className="translationHistory" key={i}>
-            <h4>
-              {dateAndTimeConversion(history[i].createdAt)}
-            </h4>
-
-          <div className="codeHistory">
-
-              <div className="entrySource">
-                <h5>
-                  Source Code ({history[i].Source_language})
-                </h5>
-                <p>
-                  {history[i].original_code}
-                </p>
-              </div>
-
-              <div className="entryDest">
-                <h5>
-                  Converted Code ({history[i].Desired_language})
-                </h5>
-                <p>
-                  {history[i].converted_code}
-                </p>
-              </div>
-            </div>
-
-            <div className="historyEntryOptions">
-              <button id="translateAgain" onClick={() => loadInputAndTranslatedCode(setInputCode, setTranslatedCode, history[i].original_code, history[i].converted_code)}> Translate again </button>
-            <button id="removeEntry">
-            <FontAwesomeIcon id="trashIcon" icon={faTrashCan} size="2x"/>
-            </button>
-            </div>
+        {history.length === 0 ? (
+          <div className="emptyHistory">
+            <img id="emptyPicture" src={emptybox} alt="History empty" style={{ width: '70%', height: '70%' }}/>
+            <a href="https://www.freepik.com/icons/empty" id="emptyCredit">Icon by Ghozi Muhtarom</a>
+            <h1 id="emptyText">No past translations</h1>
           </div>
-        ))}
+        ) : (
+          <>
+            <div className="historyOptions">
+              {/* sort asc/desc */}
+              <button>
+                <FontAwesomeIcon id="ascdsc" icon={ascend === 1 ? faArrowUp : faArrowDown} onClick={() => setAscend(ascend * -1)} />
+              </button>
+
+              {/* sort by */}
+              <select id="sort" onChange={changeSort}>
+                <option value=""> Sort By... </option>
+                <option value="Date"> Date </option>
+                <option value="Source"> Source Language </option>
+                <option value="Destination"> Destination Language </option>
+              </select>
+
+              {/* filter by */}
+              <select id="filter" onChange={changeFilter}>
+                <option value=""> Filter By... </option>
+                <option value="Date"> Date </option>
+                <option value="Source"> Source Language </option>
+                <option value="Destination"> Destination Language </option>
+              </select>
+
+              {/* clear all history */}
+              <button id="clearAll" className="ripple">Clear all history</button>
+            </div>
+
+            <div>
+              {history.map((historyLabel, i) => (
+                <div className="translationHistory" key={i}>
+                  <h4>
+                    {dateAndTimeConversion(history[i].createdAt)}
+                  </h4>
+
+                  <div className="codeHistory">
+
+                    <div className="entrySource">
+                      <h5>
+                        Source Code ({history[i].Source_language})
+                      </h5>
+                      <p>
+                        {history[i].original_code}
+                      </p>
+                    </div>
+
+                    <div className="entryDest">
+                      <h5>
+                        Converted Code ({history[i].Desired_language})
+                      </h5>
+                      <p>
+                        {history[i].converted_code}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="historyEntryOptions">
+                    <button id="translateAgain" onClick={() => loadInputAndTranslatedCode(setInputCode, setTranslatedCode, history[i].original_code, history[i].converted_code)}> Translate again </button>
+                    <button id="removeEntry">
+                      <FontAwesomeIcon id="trashIcon" icon={faTrashCan} size="2x" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </Drawer>
     </>
-  )
+  );
 }
 
 export default History
